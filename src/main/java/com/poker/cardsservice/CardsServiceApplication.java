3310d3e3e87d95612a22170e85340edf3e459ee8
Tomek <tomaszkucharski95@gmail.com>
2022-01-08 14:59:29 +0100

    initial commit

---
+++ src/main/java/com/poker/cardsservice/CardsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.poker.cardsservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class CardsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/poker/cardsservice/model/Deck.java
@@ -0,0 +1,35 @@
+package com.poker.cardsservice.model;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import lombok.Value;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Value
+public class Deck {
+    Set<Card> cards;
+
+    public Deck() {
+        this.cards = Arrays.stream(Card.Color.values())
+                .map(color -> Arrays.stream(Card.Spade.values())
+                        .map(spade -> new Card(spade, color))
+                        .collect(Collectors.toSet()))
+                .flatMap(Set::stream)
+                .collect(Collectors.toSet());
+    }
+
+    public Deck(final Set<Card> cards) {
+        this.cards = cards;
+    }
+
+    public Tuple2<Set<Card>, Deck> getTopCards(int count) {
+        List<Card> cardsList = new ArrayList<>(cards);
+        Collections.shuffle(cardsList);
+        return Tuple.of(
+                Set.copyOf(cardsList.subList(0, count)),
+                new Deck(Set.copyOf(cardsList.subList(count, cardsList.size())))
+        );
+    }
+}
